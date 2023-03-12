5b363721b0fd9f98739d584ffdbdc388bdf19ac2
paingpyaethu <paingpyaethu@gmail.com>
2023-03-12 15:04:45 +0630

    Setting up react navigation

--- RnTakeHomeChallange/android/app/src/main/java/com/rntakehomechallange/MainActivity.java
+++ RnTakeHomeChallange/android/app/src/main/java/com/rntakehomechallange/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rntakehomechallange;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RnTakeHomeChallange";
+  }
+
+  // for react-native-screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
